5aff9f91fd033d1edee19076341adc88b229df4f
linyyyang <linyyyang@tencent.com>
2021-11-25 11:23:48 +0800

    🦄 refactor:

--- src/inject.ts
+++ src/inject.ts
@@ -3,14 +3,56 @@
  * @Usage: 
  * @Author: richen
  * @Date: 2021-11-10 16:58:57
- * @LastEditTime: 2021-11-23 14:45:29
+ * @LastEditTime: 2021-11-25 11:20:27
  */
 import * as Helper from "koatty_lib";
 import { IOCContainer, RecursiveGetMetadata } from "koatty_container";
 import { Koatty, KoattyContext } from "koatty_core";
-// import { DefaultLogger as Logger } from "koatty_logger";
-import { checkParams, paramterTypes, PARAM_CHECK_KEY, PARAM_RULE_KEY } from "koatty_validation";
+import { DefaultLogger as Logger } from "koatty_logger";
+import { paramterTypes, PARAM_CHECK_KEY, PARAM_RULE_KEY } from "koatty_validation";
 import { CONTROLLER_ROUTER, PARAM_KEY, ROUTER_KEY } from "./index";
+import { getParamter } from "./params";
+
+
+/**
+ * Inject ParameterDecorator
+ * @param fn 
+ */
+export const Inject = (fn: Function, name: string): ParameterDecorator => {
+    return (target: Object, propertyKey: string, descriptor: number) => {
+        const targetType = IOCContainer.getType(target);
+        if (targetType !== "CONTROLLER") {
+            throw Error(`${name} decorator is only used in controllers class.`);
+        }
+        // 获取成员类型
+        // const type = Reflect.getMetadata("design:type", target, propertyKey);
+        // 获取成员参数类型
+        const paramTypes = Reflect.getMetadata("design:paramtypes", target, propertyKey);
+        // 获取成员返回类型
+        // const returnType = Reflect.getMetadata("design:returntype", target, propertyKey);
+        // 获取所有元数据 key (由 TypeScript 注入)
+        // const keys = Reflect.getMetadataKeys(target, propertyKey);
+        let type = (paramTypes[descriptor] && paramTypes[descriptor].name) ? paramTypes[descriptor].name : "object";
+        let isDto = false;
+        //DTO class
+        if (!(Helper.toString(type) in paramterTypes)) {
+            type = IOCContainer.getIdentifier(paramTypes[descriptor]);
+            // reg to IOC container
+            // IOCContainer.reg(type, paramTypes[descriptor]);
+            isDto = true;
+        }
+
+        IOCContainer.attachPropertyData(PARAM_KEY, {
+            name: propertyKey,
+            fn,
+            index: descriptor,
+            type,
+            isDto
+        }, target, propertyKey);
+        return descriptor;
+
+    };
+};
 
 /**
  * controller handler
@@ -90,7 +132,7 @@ export function injectParam(app: Koatty, target: any, instance?: any) {
     const argsMetaObj: any = {};
     for (const meta in metaDatas) {
         if (instance[meta] && instance[meta].length <= metaDatas[meta].length) {
-            // Logger.Debug(`Register inject ${IOCContainer.getIdentifier(target)} param key: ${Helper.toString(meta)} => value: ${JSON.stringify(metaDatas[meta])}`);
+            Logger.Debug(`Register inject param key ${IOCContainer.getIdentifier(target)}: ${Helper.toString(meta)} => value: ${JSON.stringify(metaDatas[meta])}`);
 
             // cover to obj
             const data = (metaDatas[meta] ?? []).sort((a: any, b: any) => a.index - b.index);
@@ -102,6 +144,9 @@ export function injectParam(app: Koatty, target: any, instance?: any) {
                         validMetaObj[v.index] = it;
                     }
                 });
+                if (v.type) {
+                    v.type = v.isDto ? v.type : (v.type).toLowerCase();
+                }
             });
             argsMetaObj[meta] = {
                 valids: validMetaObj,
@@ -113,73 +158,3 @@ export function injectParam(app: Koatty, target: any, instance?: any) {
     return argsMetaObj;
 }
 
-
-/**
- * Convert paramter types and valid check.
- *
- * @param {Koatty} app
- * @param {KoattyContext} ctx
- * @param {any[]} params
- * @returns
- */
-async function getParamter(app: Koatty, ctx: KoattyContext, ctlParams: any = {}) {
-    //convert type
-    const params = ctlParams.data ?? [];
-    const validRules = ctlParams.valids ?? {};
-    const dtoCheck = ctlParams.dtoCheck || false;
-    const props: any[] = params.map(async (v: any, k: number) => {
-        let value: any = null;
-        if (v.fn && Helper.isFunction(v.fn)) {
-            value = await v.fn(ctx);
-        }
-        // check params
-        return checkParams(value, {
-            index: k,
-            isDto: v.isDto,
-            type: v.type,
-            validRules,
-            dtoCheck,
-        });
-    });
-    return Promise.all(props);
-}
-
-/**
- * Inject ParameterDecorator
- * @param fn 
- */
-export const Inject = (fn: Function, name: string): ParameterDecorator => {
-    return (target: Object, propertyKey: string, descriptor: number) => {
-        const targetType = IOCContainer.getType(target);
-        if (targetType !== "CONTROLLER") {
-            throw Error(`${name} decorator is only used in controllers class.`);
-        }
-        // 获取成员类型
-        // const type = Reflect.getMetadata("design:type", target, propertyKey);
-        // 获取成员参数类型
-        const paramTypes = Reflect.getMetadata("design:paramtypes", target, propertyKey);
-        // 获取成员返回类型
-        // const returnType = Reflect.getMetadata("design:returntype", target, propertyKey);
-        // 获取所有元数据 key (由 TypeScript 注入)
-        // const keys = Reflect.getMetadataKeys(target, propertyKey);
-        let type = (paramTypes[descriptor] && paramTypes[descriptor].name) ? paramTypes[descriptor].name : "object";
-        let isDto = false;
-        //DTO class
-        if (!(Helper.toString(type) in paramterTypes)) {
-            type = IOCContainer.getIdentifier(paramTypes[descriptor]);
-            // reg to IOC container
-            // IOCContainer.reg(type, paramTypes[descriptor]);
-            isDto = true;
-        }
-
-        IOCContainer.attachPropertyData(PARAM_KEY, {
-            name: propertyKey,
-            fn,
-            index: descriptor,
-            type,
-            isDto
-        }, target, propertyKey);
-        return descriptor;
-
-    };
-};
---
+++ src/params.ts
@@ -0,0 +1,91 @@
+/*
+ * @Description: 
+ * @Usage: 
+ * @Author: richen
+ * @Date: 2021-11-24 23:21:26
+ * @LastEditTime: 2021-11-25 11:23:06
+ */
+import { Koatty, KoattyContext } from "koatty_core";
+import * as Helper from "koatty_lib";
+import { DefaultLogger as Logger } from "koatty_logger";
+import {
+    ClassValidator, convertParamsType, checkParamsType,
+    plainToClass, ValidatorFuncs
+} from "koatty_validation";
+
+/**
+ * Parameter binding assignment.
+ *
+ * @param {Koatty} app
+ * @param {KoattyContext} ctx
+ * @param {any[]} params
+ * @returns
+ */
+export async function getParamter(app: Koatty, ctx: KoattyContext, ctlParams: any = {}) {
+    //convert type
+    const params = ctlParams.data ?? [];
+    const validRules = ctlParams.valids ?? {};
+    const dtoCheck = ctlParams.dtoCheck || false;
+    const props: any[] = params.map(async (v: any, k: number) => {
+        let value: any = null;
+        if (v.fn && Helper.isFunction(v.fn)) {
+            value = await v.fn(ctx);
+        }
+        // check params
+        return checkParams(app, value, {
+            index: k,
+            isDto: v.isDto,
+            type: v.type,
+            validRules,
+            dtoCheck,
+        });
+    });
+    return Promise.all(props);
+}
+
+/**
+ *
+ *
+ * @interface ParamOptions
+ */
+interface ParamOptions {
+    index: number;
+    isDto: boolean;
+    type: string;
+    validRules: any[];
+    dtoCheck: boolean;
+}
+
+/**
+ * Parameter binding assignment and rule verification.
+ * If the input parameter type is inconsistent with the calibration, 
+ * it will cause the parameter type conversion
+ *
+ * @param {Koatty} app
+ * @param {*} value
+ * @param {ParamOptions} opt
+ * @returns {*}  
+ */
+async function checkParams(app: Koatty, value: any, opt: ParamOptions) {
+    if (opt.isDto) {
+        // DTO class
+        const clazz = app.container.getClass(opt.type, "COMPONENT");
+        if (clazz) {
+            if (opt.dtoCheck) {
+                value = await ClassValidator.valid(clazz, value, true);
+            } else {
+                value = plainToClass(clazz, value, true);
+            }
+        }
+    } else {
+        value = convertParamsType(value, opt.type);
+        //@Valid()
+        if (opt.validRules[opt.index]) {
+            const { type, rule, message } = opt.validRules[opt.index];
+            if (type && rule) {
+                ValidatorFuncs(`${opt.index}`, value, type, rule, message, false);
+            }
+        }
+    }
+    return value;
+}
