954e0a2e41c712452e78f84ed21ec2cd4467f728
linyyyang <linyyyang@tencent.com>
2021-12-13 17:33:52 +0800

    🐞 fix:

--- test/protobuf.test.ts
+++ test/protobuf.test.ts
@@ -3,17 +3,18 @@
  * @Usage: 
  * @Author: richen
  * @Date: 2021-11-10 15:04:26
- * @LastEditTime: 2021-12-13 16:52:36
+ * @LastEditTime: 2021-12-13 17:32:49
  */
 import path from "path";
-import { LoadProto } from "koatty_proto";
+import { ListServices, LoadProto } from "koatty_proto";
 
 
 const protoFile = path.resolve("./test/example.proto");
 
 describe("protobuf", () => {
     test("LoadProto", async function () {
-        const res = await LoadProto(protoFile);
+        const pdef = await LoadProto(protoFile);
+        const res = ListServices(pdef);
         expect(res.length > 0).toEqual(true);
         console.log(res);
 
