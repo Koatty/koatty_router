5a6a5d0b3cc1ed881fa88724095e5a85b1ada235
linyyyang <linyyyang@tencent.com>
2025-04-06 23:16:22 +0800

    feat: add connection pooling and batch processing to gRPC router with stream handling support

--- src/router/grpc.ts
+++ src/router/grpc.ts
@@ -3,7 +3,7 @@
  * @Usage:
  * @Author: richen
  * @Date: 2021-06-29 14:10:30
- * @LastEditTime: 2025-03-14 15:09:42
+ * @LastEditTime: 2025-04-06 22:56:00
  */
 import { UntypedHandleCall } from "@grpc/grpc-js";
 import { IOCContainer } from "koatty_container";
@@ -16,18 +16,115 @@ import {
 import * as Helper from "koatty_lib";
 import { DefaultLogger as Logger } from "koatty_logger";
 import { ListServices, LoadProto } from "koatty_proto";
-import { detectServer, Handler, injectParamMetaData, injectRouter, ParamMetadata } from "../utils/inject";
+import { injectParamMetaData, injectRouter, ParamMetadata } from "../utils/inject";
 import { parsePath } from "../utils/path";
 import { RouterOptions } from "./router";
+import { Handler } from "../utils/handler";
 
 /**
  * GrpcRouter Options
- *
- * @export
- * @interface GrpcRouterOptions
  */
 export interface GrpcRouterOptions extends RouterOptions {
   protoFile: string;
+  poolSize?: number;
+  batchSize?: number;
+}
+
+/**
+ * Connection pool for gRPC clients
+ */
+class GrpcConnectionPool {
+  private pool: Map<string, any[]>;
+  private maxSize: number;
+
+  constructor(maxSize: number = 10) {
+    this.pool = new Map();
+    this.maxSize = maxSize;
+  }
+  
+  /**
+   * Get connection from pool
+   */
+  get(serviceName: string): any | null {
+    if (!this.pool.has(serviceName) || this.pool.get(serviceName)?.length === 0) {
+      return null;
+    }
+    return this.pool.get(serviceName)?.pop();
+  }
+
+  /**
+   * Release connection back to pool
+   */
+  release(serviceName: string, connection: any): void {
+    if (!this.pool.has(serviceName)) {
+      this.pool.set(serviceName, []);
+    }
+    if (this.pool.get(serviceName)?.length < this.maxSize) {
+      this.pool.get(serviceName)?.push(connection);
+    }
+  }
+
+  /**
+   * Create new connection
+   */
+  private create(_serviceName: string, _options: any): any {
+    // Implementation depends on your gRPC client library
+  }
+}
+
+/**
+ * Batch processor for gRPC requests
+ */
+class GrpcBatchProcessor {
+  private batchSize: number;
+  private batchQueue: Map<string, any[]>;
+  private batchTimers: Map<string, NodeJS.Timeout>;
+
+  constructor(batchSize: number = 10) {
+    this.batchSize = batchSize;
+    this.batchQueue = new Map();
+    this.batchTimers = new Map();
+  }
+
+  /**
+   * Add request to batch
+   */
+  addRequest(serviceName: string, request: any): Promise<any> {
+    return new Promise((resolve, reject) => {
+      if (!this.batchQueue.has(serviceName)) {
+        this.batchQueue.set(serviceName, []);
+      }
+
+      const queue = this.batchQueue.get(serviceName)!;
+      queue.push({ request, resolve, reject });
+
+      // Process batch if size reached
+      if (queue.length >= this.batchSize) {
+        this.processBatch(serviceName);
+      } else if (!this.batchTimers.has(serviceName)) {
+        // Start timer for batch processing
+        this.batchTimers.set(serviceName, setTimeout(() => {
+          this.processBatch(serviceName);
+        }, 100));
+      }
+    });
+  }
+
+  /**
+   * Process batch of requests
+   */
+  private processBatch(serviceName: string): void {
+    const queue = this.batchQueue.get(serviceName);
+    if (!queue || queue.length === 0) return;
+
+    clearTimeout(this.batchTimers.get(serviceName)!);
+    this.batchTimers.delete(serviceName);
+    // Process batch here
+    // This would call the actual gRPC batch endpoint
+    // Then resolve/reject each promise in the queue
+
+    this.batchQueue.delete(serviceName);
+  }
 }
 
 /**
@@ -36,32 +133,32 @@ export interface GrpcRouterOptions extends RouterOptions {
  * @interface CtlInterface
  */
 interface CtlInterface {
-  [path: string]: CtlProperty
-}
-/**
- * CtlProperty
- *
- * @interface CtlProperty
- */
-interface CtlProperty {
-  name: string;
-  ctl: Function;
-  method: string;
-  params: ParamMetadata[];
+  [path: string]: {
+    name: string;
+    ctl: Function;
+    method: string;
+    params: ParamMetadata[];
+  }
 }
 
 export class GrpcRouter implements KoattyRouter {
   readonly protocol: string;
   options: GrpcRouterOptions;
   router: Map<string, RouterImplementation>;
+  private connectionPool: GrpcConnectionPool;
+  private batchProcessor: GrpcBatchProcessor;
 
-  constructor(app: Koatty, options?: RouterOptions) {
-    options.ext = options.ext || {};
+  constructor(app: Koatty, options: RouterOptions = { protocol: "grpc", prefix: "" }) {
+    const ext = options.ext || {};
     this.options = {
       ...options,
-      protoFile: options.ext.protoFile,
+      protoFile: ext.protoFile,
+      poolSize: ext.poolSize || 10,
+      batchSize: ext.batchSize || 10
     };
     this.router = new Map();
+    this.connectionPool = new GrpcConnectionPool(this.options.poolSize);
+    this.batchProcessor = new GrpcBatchProcessor(this.options.batchSize);
   }
 
   /**
@@ -71,12 +168,10 @@ export class GrpcRouter implements KoattyRouter {
    * @returns 
    */
   SetRouter(name: string, impl?: RouterImplementation) {
-    if (Helper.isEmpty(name)) {
-      return;
-    }
+    if (Helper.isEmpty(name)) return;
     this.router.set(name, {
-      service: impl.service,
-      implementation: impl.implementation
+      service: impl?.service,
+      implementation: impl?.implementation
     });
   }
 
@@ -91,71 +186,84 @@ export class GrpcRouter implements KoattyRouter {
   }
 
   /**
+   * Stream handler for gRPC
+   */
+  private handleStream(call: IRpcServerCall<any, any>, callback: IRpcServerCallback<any>, handler: Function) {
+    const stream = call as any;
+    if (stream.on) {
+      stream.on('data', (data: any) => handler(data));
+      stream.on('end', () => callback(null, {}));
+      stream.on('error', (err: Error) => callback(err));
+      
+      // 如果是可写流，处理初始请求
+      if (stream.request) {
+        handler(stream.request);
+      }
+    }
+  }
+
+  /**
    * LoadRouter
    *
    * @memberof Router
    */
   async LoadRouter(app: Koatty, list: any[]) {
     try {
-      // load proto files
       const pdef = LoadProto(this.options.protoFile);
       const services = ListServices(pdef);
-
       const ctls: CtlInterface = {};
+
       for (const n of list) {
         const ctlClass = IOCContainer.getClass(n, "CONTROLLER");
-        // inject router
         const ctlRouters = injectRouter(app, ctlClass, this.options.protocol);
-        if (!ctlRouters) {
-          continue;
-        }
-        // inject param
+        if (!ctlRouters) continue;
+
         const ctlParams = injectParamMetaData(app, ctlClass, this.options.payload);
-
         for (const router of Object.values(ctlRouters)) {
-          const { method, path } = router;
-          const params = ctlParams[method];
-
-          ctls[path] = {
+          ctls[router.path] = {
             name: n,
             ctl: ctlClass,
-            method,
-            params,
-          }
+            method: router.method,
+            params: ctlParams[router.method],
+          };
         }
       }
 
-      // 循环匹配服务绑定路由
       for (const si of services) {
-        const serviceName = si.name;
-        // Verifying
         if (!si.service || si.handlers.length === 0) {
-          Logger.Warn('Ignore', serviceName, 'which is an empty service');
+          Logger.Warn('Ignore', si.name, 'which is an empty service');
           continue;
         }
-        const impl: { [key: string]: UntypedHandleCall } = {};
+
+        const impl: Record<string, UntypedHandleCall> = {};
         for (const handler of si.handlers) {
           const path = parsePath(handler.path);
-          if (ctls[path]) {
-            const ctlItem = ctls[path];
-            Logger.Debug(`Register request mapping: ["${path}" => ${ctlItem.name}.${ctlItem.method}]`);
-            impl[handler.name] = (call: IRpcServerCall<any, any>, callback: IRpcServerCallback<any>) => {
-              return app.callback("grpc", (ctx) => {
-                const ctl = IOCContainer.getInsByClass(ctlItem.ctl, [ctx]);
-                return Handler(app, ctx, ctl, ctlItem.method, ctlItem.params);
-              })(call, callback);
-            };
-          }
+          const ctlItem = ctls[path];
+          if (!ctlItem) continue;
+
+          Logger.Debug(`Register request mapping: ["${path}" => ${ctlItem.name}.${ctlItem.method}]`);
+          impl[handler.name] = (call: IRpcServerCall<any, any>, callback: IRpcServerCallback<any>) => {
+            if ('stream' in call) {
+              return this.handleStream(call, callback, (data: any) => {
+                app.callback("grpc", (ctx) => {
+                  const ctl = IOCContainer.getInsByClass(ctlItem.ctl, [ctx]);
+                  return Handler(app, ctx, ctl, ctlItem.method, ctlItem.params);
+                })(data, callback);
+              });
+            }
+
+            return app.callback("grpc", (ctx) => {
+              const ctl = IOCContainer.getInsByClass(ctlItem.ctl, [ctx]);
+              return Handler(app, ctx, ctl, ctlItem.method, ctlItem.params);
+            })(call, callback);
+          };
         }
-        // set router
-        this.SetRouter(serviceName, { service: si.service, implementation: impl });
-        // RegisterService
-        const server = detectServer(app.server, "grpc");
-        server?.RegisterService({ service: si.service, implementation: impl });
+
+        this.SetRouter(si.name, { service: si.service, implementation: impl });
+        app.server?.RegisterService({ service: si.service, implementation: impl });
       }
     } catch (err) {
       Logger.Error(err);
     }
   }
-
 }
